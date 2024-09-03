5a66799195ba4766aa5354476bc5204fab2aebe4
Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>
2024-09-02 20:40:34 -0700

    Add files via upload

--- main2.py
+++ main2.py
@@ -13,3 +13,4 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 for voice in voices:
     print(f"ID: {voice.id}\nName: {voice.name}\nGender: {voice.gender}\nLanguage: {voice.languages}\n")
+
