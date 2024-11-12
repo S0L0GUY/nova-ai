7b1b7394716f1f1c28ffaa7da44055b4bcb2adec
Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>
2024-11-12 12:22:00 -0800

    Added Constants File

---
+++ constants.py
@@ -0,0 +1,10 @@
+# Netwoek Settings
+LOCAL_IP = "192.168.0.195" # Your computers local IP
+VRC_PORT = 9000 # VR Chat VRC_PORT, 9000 is the default
+
+# Audio Settings
+AUDIO_OUTPUT_INDEX = 7 # The index of the audio output device (VB-Audio Cable B)
+
+# LM Settings
+MODEL_ID = "lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF" # The model ID of the LM you want to use
+LM_TEMPERATURE = 0.5 # The temperature of the LM
