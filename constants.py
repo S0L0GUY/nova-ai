c03eb8430e0db701683774d856ba68388f32431f
Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>
2024-12-04 20:41:25 -0800

    Updated Constants

--- constants.py
+++ constants.py
@@ -7,5 +7,8 @@ AUDIO_INPUT_INDEX = 0
 
 TTS_OUTPUT_DIR = 'tts_outputs'
 
+LM_TEMPERATURE = 0.7
+MODEL_ID = "lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF"
+
 HISTORY_PATH = 'json_files/history.json'
 MOOD_PATH = 'json_files/mood.json'
